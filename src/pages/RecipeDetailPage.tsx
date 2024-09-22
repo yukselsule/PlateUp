27a9aa883c6a20fcdc0952e748e0cec57538b705
yukselsule <sulecantekin@gmail.com>
2024-09-22 13:23:20 +0300

    feat: 🎸 created RecipeDetailsPage

--- src/pages/RecipeDetailPage.tsx
+++ src/pages/RecipeDetailPage.tsx
@@ -1,7 +1,117 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks/hooks";
+import { getRecipeById, selectSelectedRecipe } from "../slices/recipesSlice";
+
+const safeQuantity = (quantity: number | undefined, yieldValue: number) => {
+  return quantity ? (quantity / yieldValue).toFixed(2) : "0.00";
+};
+
 const RecipeDetailPage = () => {
+  const { recipeId } = useParams();
+  const dispatch = useAppDispatch();
+  const recipe = useAppSelector(selectSelectedRecipe);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (recipeId) {
+      dispatch(getRecipeById(recipeId))
+        .unwrap()
+        .finally(() => setLoading(false));
+    }
+  }, [dispatch, recipeId]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+
+  const {
+    ENERC_KCAL,
+    FAT,
+    FASAT,
+    FAMS,
+    FAPU,
+    FATRN,
+    CHOCDF,
+    SUGAR,
+    FIBTG,
+    PROCNT,
+    NA,
+    CHOLE,
+    K,
+  } = recipe.totalNutrients;
+  const yieldValue = recipe?.yield || 1;
+
   return (
     <div>
-      <h1>RecipeDetailPage</h1>
+      <h1>{recipe.label}</h1>
+      <img src={recipe.image} alt={recipe.label} />
+      <h3>Ingredients</h3>
+      <ul>
+        {recipe.ingredientLines.map((ingredient, i) => (
+          <li key={i}>{ingredient}</li>
+        ))}
+      </ul>
+
+      <h3>Nutritional Information</h3>
+      <ul>
+        <li>
+          Energy: {safeQuantity(ENERC_KCAL?.quantity, yieldValue)}{" "}
+          {ENERC_KCAL?.unit}
+        </li>
+        <li>
+          Fat: {safeQuantity(FAT?.quantity, yieldValue)} {FAT?.unit}
+          <ul>
+            <li>
+              Saturated Fat: {safeQuantity(FASAT?.quantity, yieldValue)}{" "}
+              {FASAT?.unit}
+            </li>
+            <li>
+              Trans Fat: {safeQuantity(FATRN?.quantity, yieldValue)}{" "}
+              {FATRN?.unit}
+            </li>
+            <li>
+              Monounsaturated Fat: {safeQuantity(FAMS?.quantity, yieldValue)}{" "}
+              {FAMS?.unit}
+            </li>
+            <li>
+              Polyunsaturated Fat: {safeQuantity(FAPU?.quantity, yieldValue)}{" "}
+              {FAPU?.unit}
+            </li>
+          </ul>
+        </li>
+        <li>
+          Carbs: {safeQuantity(CHOCDF?.quantity, yieldValue)} {CHOCDF?.unit}
+          <ul>
+            <li>
+              Sugar: {safeQuantity(SUGAR?.quantity, yieldValue)} {SUGAR?.unit}
+            </li>
+            <li>
+              Fiber: {safeQuantity(FIBTG?.quantity, yieldValue)} {FIBTG?.unit}
+            </li>
+          </ul>
+        </li>
+        <li>
+          Protein: {safeQuantity(PROCNT?.quantity, yieldValue)} {PROCNT?.unit}
+        </li>
+        <li>
+          Sodyum: {safeQuantity(NA?.quantity, yieldValue)} {NA?.unit}
+        </li>
+        <li>
+          Cholesterol: {safeQuantity(CHOLE?.quantity, yieldValue)} {CHOLE?.unit}
+        </li>
+        <li>
+          Potassium: {safeQuantity(K?.quantity, yieldValue)} {K?.unit}
+        </li>
+      </ul>
+
+      <a href={recipe.url} target="_blank" rel="noopener noreferrer">
+        Full recipe details
+      </a>
     </div>
   );
 };
